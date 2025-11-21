d5a5b0534299bb86e07353285132cf6c7967423c
martinstier <mastier@umich.edu>
2025-11-21 15:09:14 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Hi I am Martin.</h1>
+<ul>
+  <li>About</li>
+  <li>About</li>
+  <li>About</li>
+</ul>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
