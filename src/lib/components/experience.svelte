efdc2dbd76985cd291bb77dc2f940d9ea692a38e
martinstier <mastier@umich.edu>
2025-11-25 23:35:07 -0800

    actually calling it

--- src/lib/components/experience.svelte
+++ src/lib/components/experience.svelte
@@ -6,11 +6,11 @@
   <p>
     Martín is currently a software engineer at <span
       class="bg-[#0385FF] text-[#EAFFD9]">CHEPS</span
-    >. He develops and maintains staff management software for state-run
-    psychiatric hospitals in Michigan. Martín has previously interned at
-    <span class="bg-[#0385FF] text-[#EAFFD9]">PromethistAI</span>; taught
+    >. He develops staff management software for state-run psychiatric hospitals
+    in Michigan. Martín has previously interned at
+    <span class="bg-[#0385FF] text-[#EAFFD9]">PromethistAI</span>, taught
     <span class="bg-[#0385FF] text-[#EAFFD9]">physics</span> and
-    <span class="bg-[#0385FF] text-[#EAFFD9]">calculus</span>; and worked at the
+    <span class="bg-[#0385FF] text-[#EAFFD9]">calculus</span>, and worked at the
     <span class="bg-[#0385FF] text-[#EAFFD9]"
       >Institute for Social Research</span
     >.
--- src/lib/components/navigation.svelte
+++ src/lib/components/navigation.svelte
@@ -4,12 +4,27 @@
   export let className: string = "";
 
   const images = [
-    "/images/image0.jpg",
-    "/images/image1.jpeg",
-    "/images/image2.jpg",
+    "/images/img0.JPG",
+    // "/images/img1.jpeg",
+    // "/images/img2.JPG",
+    "/images/img3.JPG",
+    "/images/img4.JPG",
+    // "/images/img5.JPG",
+    // "/images/img6.JPG",
+    // "/images/img7.jpg",
+    "/images/img8.jpg",
+    "/images/img9.JPG",
+    "/images/img10.jpg",
+    "/images/img11.JPG",
+    "/images/img12.jpeg",
+    "/images/img13.jpeg",
+    // "/images/img14.JPG",
+    // "/images/img15.JPG",
+    "/images/img16.jpeg",
+    // "/images/img17.jpg",
   ];
 
-  let current = 0;
+  let current = 4;
 
   function next() {
     current = (current + 1) % images.length;
@@ -23,7 +38,7 @@
 
   function startRotation() {
     if (!interval) {
-      interval = setInterval(next, 100);
+      interval = setInterval(next, 150);
     }
   }
 
@@ -32,14 +47,23 @@
       clearInterval(interval);
       interval = null;
     }
+
+    current = 4;
   }
 
   const sections = ["about", "experience", "projects", "directory"];
 
   function scrollToSection(id: string) {
     const el = document.getElementById(id);
+
     if (el) {
       el.scrollIntoView({ behavior: "smooth", block: "start" });
+      //   const y = el.getBoundingClientRect().top + window.scrollY - 8;
+
+      //   window.scrollTo({
+      //     top: y,
+      //     behavior: "smooth",
+      //   });
     }
   }
 </script>
